bac03f29000553f6abf810c2d8389858990640ff
Kihikimaros <82409361+Kihikimaros@users.noreply.github.com>
2021-09-27 19:23:19 +0500

    Initial commit

---
+++ src/main/java/Tests/AppTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.jetbrains.annotations.TestOnly;
+
+public class AppTest {
+
+    @TestOnly
+    public void shouldAnswerWithTrue() {
+        System.out.println("test1");
+    }
+
+
+        @TestOnly
+        public void shouldAnswerWithTrue2() {
+            System.out.println("test2");
+        }
+}
---
+++ src/main/java/Tests/IntegrationTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.jetbrains.annotations.TestOnly;
+
+public class IntegrationTest {
+
+    @TestOnly
+    public void shouldAnswerWithTrue() {
+        System.out.println("Integration test1");
+    }
+
+
+    @TestOnly
+    public void shouldAnswerWithTrue2() {
+        System.out.println("Integration test2");
+    }
+}
